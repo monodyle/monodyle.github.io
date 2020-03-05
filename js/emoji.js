e54d91dcdf95103d0be806648cb9c4c13af6f177
Monody <monodylh@gmail.com>
2020-03-06 04:04:29 +0700

    table of content fixed; new emoji

--- js/emoji.js
+++ js/emoji.js
@@ -5,6 +5,7 @@ const emojiMap = {
   surrender: "/assets/emoji/surrender.png",
   lookdown: "/assets/emoji/look_down.png",
   okay: "/assets/emoji/okay.png",
+  argggg: "/assets/emoji/argggg.png",
 }
 
 const pattern = /:([\w-_]+):/g
@@ -13,7 +14,11 @@ let container = document.querySelectorAll(".article-body > p, .article-body > ul
 const emojiParse = (regex, content) => {
   let result = ""
   while (result = regex.exec(content)) {
-    if (emojiMap[result[1]]) content = content.replace(result[0], `<img src="${emojiMap[result[1]]}" class="emoji" />`)
+    if (emojiMap[result[1]])
+      content = content.replace(
+        result[0],
+        `<img src="${emojiMap[result[1]]}" class="emoji" />`
+      )
   }
   return content
 }
