f611dcc3f5e54916a007ad49aea538e194c3bbc6
Monody <monodylh@gmail.com>
2020-03-15 00:10:31 +0700

    toc must shown on >= 2 item and added title

--- js/toc.js
+++ js/toc.js
@@ -1,8 +1,8 @@
 const start_toc_module = function() {
   const replaceATag = str => `${str.replace('</a>','').replace(/id=\".*\"/,'').replace(/class=\".*\"/,'').replace(/aria-hidden=\".*\"/,'')}</a>`;
-  let toc_headings = [].slice.call(document.querySelectorAll('.article-body h2, .article-body h3, .article-body h4, .article-body h5, .article-body h6'));
-  let toc_items = toc_headings.map(item => `<li><${item.localName}><a href="#${item.id}">${replaceATag(item.innerHTML)}</a></${item.localName}></li>`);
-  if (toc_items.length <= 0) return;
+  let toc_headings = [].slice.call(document.querySelectorAll('.article-body h2, .article-body h3, .article-body h4'));
+  let toc_items = toc_headings.map(item => `<li><${item.localName}><a href="#${item.id}" title="${replaceATag(item.innerHTML).replace('</a>', '')}">${replaceATag(item.innerHTML)}</${item.localName}></li>`);
+  if (toc_items.length <= 1) return;
   let toc_html_template = `<div class="toc-container"><ul>${toc_items.join('')}</ul></div>`;
   let toc_element_container = document.createElement('div');
   toc_element_container.innerHTML = toc_html_template;
