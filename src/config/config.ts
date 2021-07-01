816fa7396759aef2b6119bcef62bf403d121da21
monodyle <monodylh@gmail.com>
2021-07-01 23:00:21 +0700

    chrone: update config.url domain

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   author: 'Monody Le',
-  url: 'https://monodyle.github.io',
+  url: 'https://minhle.space',
   title: "The Monody's Space",
   excerpt: 'Hello, just a simple guy.',
   image: '/assets/cover.png',
