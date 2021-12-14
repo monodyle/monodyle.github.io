97cb902c1f35847c4c4dead91ad737b76bc44981
monodyle <monodylh@gmail.com>
2021-12-14 17:13:17 +0700

    fix: css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from "next/app"
 import { StrictMode } from "react"
-import "~components/app/global.css"
 import { SettingsProvider } from "~components/blog/post/settings/context"
 import SEO from "~components/seo/seo"
+
+import "~components/app/global.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
